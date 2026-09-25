bba438e77b53632bb31c3375b334f7d3767c7d88
UcnacDx2 <127503808+UcnacDx2@users.noreply.github.com>
2026-09-25 11:15:26 +0800

    revert: preserve official MITM connection flow

--- proxy/socks5.go
+++ proxy/socks5.go
@@ -159,7 +159,7 @@ func (p *ProxyServer) handleSocks5Connect(ctx context.Context, writer io.Writer,
 			writer: writer,
 		}
 		_ = hijackConn.SetDeadline(time.Time{})
-		p.handleMITM(hijackConn, cr.targetHost, cr.rule, cr.dialCandidates, cr.dialAddr, cr.conn)
+		p.handleMITM(hijackConn, cr.targetHost, cr.rule, cr.dialCandidates, cr.dialAddr)
 	case "tls-rf":
 		hijackConn := &socks5HijackConn{
 			Conn:   clientConn,
